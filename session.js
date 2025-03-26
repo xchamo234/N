1720505bf0acfbe3053f5a10d40289a6c31a7674
xchamo234 <xchamonirmal9@gmail.com>
2025-03-26 15:59:51 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,9 +40,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~nV1nmBoB#MzYfrP11-6ier63RonZ67M2SHwIfAsiEf1mTC-dyVK8",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~DUdlDR4I#xOG7uTqVdfYyc69_Kiv5GjokREDSqoFdvZ1IHTzy2to",
 SUDO: process.env.SUDO || ["94777580876","94743430452","94775512050"],
 PORT: process.env.PORT || "8000",
 DATABASE_URL: process.env.DATABASE_URL === undefined ? 'postgresql://admin:npg_0leWpxvmgoh9@ep-bold-lab-a4wrf69s-pooler.us-east-1.aws.neon.tech/chamiya?sslmode=require' : process.env.DATABASE_URL,    
-FOOTER: process.env.FOOTER || "> *ＤＡＲＫＡＬＰＨＡＸＴＥＡＭ*",
+FOOTER: process.env.FOOTER || "> ᴄ.ɴ ᴍᴏᴠɪᴇ ʜᴜʙ ʙʏ 𝙭𝘾𝙝𝙖𝙢𝙤!☁️",
 };
